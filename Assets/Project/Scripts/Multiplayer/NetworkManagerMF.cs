6c93d090fd6a65783371681b1aeffa7d8090267a
ppawlinski <73852323+ppawlinski@users.noreply.github.com>
2021-10-01 12:03:05 +0200

    Cleanup
    deleting unused asset packages

---
+++ Assets/Project/Scripts/Multiplayer/NetworkManagerMF.cs
@@ -0,0 +1,13 @@
+﻿using Mirror;
+using UnityEngine;
+
+public class NetworkManagerMF : NetworkManager
+{
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        GameObject player = GetComponent<NetworkPlayerManager>().AddPlayer(playerPrefab, conn.connectionId);
+
+        player.name = $"{playerPrefab.name} [connId={conn.connectionId}]";
+        NetworkServer.AddPlayerForConnection(conn, player);
+    }
+}
---
+++ Assets/Project/Scripts/Multiplayer/NetworkPlayerManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+public class NetworkPlayerManager : MonoBehaviour
+{
+    Dictionary<int, Player> players = new Dictionary<int, Player>();
+    float playersTeam1 = 0;
+    float playersTeam2 = 0;
+    public GameObject AddPlayer(GameObject playerPrefab, int connId)
+    {
+        GameObject playerObject = Instantiate(playerPrefab);
+        int team = playersTeam1 < GameParameters.Instance.PlayersPerTeam ? 1 : 2;
+        Player player = new Player(playerObject, Player.PlayerType.Local, team);
+        players.Add(connId, player);
+        SetPlayerPosition(player);
+        return playerObject;
+    }
+
+    public void SetPlayerPosition(Player p)
+    {
+        p.PlayerObject.transform.position = new Vector3(80, 14, 5);
+    }
+}
